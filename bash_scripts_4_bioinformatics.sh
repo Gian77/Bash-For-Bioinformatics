5ec1b57ebb2d758c1189f0a7b12fa2d53e74b642
GMNB <gian.benucci@gmail.com>
2016-10-28 10:03:36 -0400

    Update bash_scripts_4_bioinformatics.sh

--- bash_scripts_4_bioinformatics.sh
+++ bash_scripts_4_bioinformatics.sh
@@ -29,9 +29,10 @@ cat sample1.fq | echo $((`wc -l`/4))
 
 
 
+
+
 ## MORE ADVANCED COMMANDS ...to be continued!
 #*******************************************#
-
 
 ## Convert a .fastq file to .fasta (NOTE: this assumes that each FASTQ entry spans only four lines as is customary)
 sed -n '1~4s/^@/>/p;2~4p' imputfilename.fastq > outputfilename.fasta
@@ -60,21 +61,17 @@ done
 ## give the mean of the lenght of the seqs contained in a .fasta file
 awk '{/>/&&++a||b+=length()}END{print b/a}' seqs.fna 
 
-
 ## Convert .fasta and .qual to .fastq 
 for i in $(ls *.fasta);
 do convert_fastaqual_fastq.py -f R -q seqs.qual -o fastq_files/
 done
 
-
 ## Creating a md5 report for a series of file
 md5sum *.fastq > md5.txt
-
 
 ## Get a histogram of sequence lengths from FASTA/Q files 
 cat imputfile.fastq | awk '{if(NR%4==2) print length($1)}' | sort -n | uniq -c
 cat imputfile.fasta | awk '{if(NR%4==0) print length($1)}' | sort -n | uniq -c
-
 
 ## Do arithmetic operations on the bash command line
 echo $((1 + 1))
@@ -85,13 +82,11 @@ echo $(((1+3) / (1+1)))
 
 echo "scale=1; 1/2" | bc ## this is for floating numbers
 
-
 ## Replace spaces in file names with underscore
 rename ' ' '_' *
 
 ## Rename sequence header using awk
 awk '/^>/{print ">whateveryouwant" ++i; next}{print}' < file.fasta
-
 
 ## add something to end of all header lines
 sed 's/>.*/>whateveryouwant/' file.fasta > outfile.fasta
@@ -103,27 +98,20 @@ sed 's/^>/>SampleA/g' INFILE.fasta>OUTFILE.fasta
 ## clean up a fasta file so only first column of the header is outputted
 awk '{print $1}' inputfile.fasta > outputfile.fasta
 
-
 ### check identical sequences with bash script and collapse them
 grep -v '>\|^$'  inputfile.fasta | sort -u > Sequence
 while read SEQUENCE; do
 grep -w -m 1 -B1 $SEQUENCE inputfile.fasta >> outputfile.fasta
 done < Sequence
 
-
 ## To remove duplicated sequences
 sed -e '/^>/s/$/@/' -e 's/^>/#/' file.fasta | tr -d '\n' | tr "#" "\n" | tr "@" "\t" | sort -u -t $'\t' -f -k 2,2  | sed -e 's/^/>/' -e 's/\t/\n/'
-
 
 ## To extract ids from a sequence file
 grep -o -E "^>\w+" file.fasta | tr -d ">"
 
-
 ## Linearize your sequences i.e. remove the sequence wrapping
 sed -e 's/\(^>.*$\)/#\1#/' file.fasta | tr -d "\r" | tr -d "\n" | sed -e 's/$/#/' | tr "#" "\n" | sed -e '/^$/d'
-
-
-
 
 ## convert tab-delimited to csv
 tr "\\t" "," < otu_table.txt > otu_table.csv
