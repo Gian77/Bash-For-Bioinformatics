9ccb541679a17e9e5850e210b8319561d36870b2
GMNB <gian.benucci@gmail.com>
2016-11-18 07:40:55 -0500

    Update SRA_download.sh

--- SRA_download.sh
+++ SRA_download.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # first download the accessions list of the SRA archive 
-# and save them into a .txt then run the script below
+# and save them into a file.txt then run the script below
 
 while read line; do
 echo $line
 fastq-dump -I --split-files $line
-done < SraAccList_Peay.txt
+done < SraAccList.txt
 
 while read line; do
 echo $line
 fastq-dump -I --split-files $line
-done < SraAccList_Jump.txt
+done < SraAccList.txt
 
 
 
