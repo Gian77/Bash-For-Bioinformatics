af594f4ace7b243fb3787ed75aa951db0fd2c6ed
GMNB <gian.benucci@gmail.com>
2017-12-15 16:56:24 -0500

    Update bash_awk.sh

--- bash_awk.sh
+++ bash_awk.sh
@@ -1,10 +1,18 @@
-
-# terminal scripts with awk
-
+##################################################
+##						       
+##       Some interesting awk scripts	         
+##                                        					
+##               Gian MN Benucci		              
+##       gian[dot]benucci[at]gmail[dot]com	   
+##						                                        
+##           East Lansing, Michigan		          
+##            July 1th, 2017 		       
+##						                                      
+##################################################
 
 # determine sequence length
 awk '/^>/ {if (seqlen){print seqlen}; print ;seqlen=0;next; } { seqlen += length($0)}END{print seqlen}' file.fa
 
-
+awk '/^>/ {$0=$0 "some random text"}1' input.fasta > output.fasta
 
 
