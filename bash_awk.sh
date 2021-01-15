c7143295d4c525497d89cfc4331cd281f582a33d
GMNB <gian.benucci@gmail.com>
2021-01-15 15:37:11 -0500

    Update bash_awk.sh

--- bash_awk.sh
+++ bash_awk.sh
@@ -15,4 +15,5 @@ awk '/^>/ {if (seqlen){print seqlen}; print ;seqlen=0;next; } { seqlen += length
 
 awk '/^>/ {$0=$0 "some random text"}1' input.fasta > output.fasta
 
-
+# change the header to a numerical sequence 1 to N number of reads in the file.fasta 
+awk '/^>/{print ">" ++i; next}{print}' < file.fasta > output.fasta
