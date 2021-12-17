b8717ab5381ca7c81451164c6d242eeedb5b6707
GMNB <gian.benucci@gmail.com>
2021-12-17 16:40:44 -0500

    Update AnacondaCommands.sh

--- AnacondaCommands.sh
+++ AnacondaCommands.sh
@@ -1,12 +1,21 @@
-#CONDA COMMANDS
+### EVERY DAY ANACONDA COMMANDS ###
 
-conda list
-conda activate py3
-
+# search for packages
 conda search -f constax
+conda search -c bioconda constax
 
+# list conda properties
+conda info --envs
+conda activate genome_work
+conda list
+
+# adding channels 
 conda config --add channels defaults
 conda config --add channels bioconda
 conda config --add channels conda-forge
 
+# create environments
+conda create --name myenv
+
+# install packages
 conda install -c bioconda constax=2.0.15=pyhdfd78af_0
